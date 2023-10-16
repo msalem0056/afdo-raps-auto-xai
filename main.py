bb52b44d2cc95e994f439c45bd9008ebec099e9c
msalem0056 <mike.f.salem@gmail.com>
2023-10-15 20:16:11 -0700

    found on documentation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from streamlit.components.v1 import iframe
-
-
 import subprocess
+import sys
 
 # Global Variable
 if 'started' not in st.session_state:
@@ -20,7 +19,8 @@ def app(title=None)-> None:
     st.write("The following is an example showcasing XAI using explainer dashboard and streamlit. The `what-if` section allows users to explore different answers for given / new instances. If the box belows says `unable to connect` please click `try again` and wait for it to refresh")
     st.components.v1.iframe("http://127.0.0.1:8050", width=None, height=900, scrolling=True)
     if not st.session_state['started']:
-        subprocess.run(["python", "explainer-dashboard.py", "&"]) 
+        #subprocess.run(["python", "explainer-dashboard.py", "&"]) 
+        subprocess.run([f"{sys.executable}", "explainer-dashboard.py"]) #for streamlit
         st.session_state['started'] = True      
 
 # Render the ExplainerDashboard in the Streamlit app
