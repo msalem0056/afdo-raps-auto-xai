48fef7e4a9250f6fa043085d54cacfd338736c2c
msalem0056 <mike.f.salem@gmail.com>
2023-10-15 20:12:38 -0700

    reverting change. issue on streamlit site

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import streamlit as st
 from streamlit.components.v1 import iframe
 
+
 import subprocess
-from sklearn.datasets import load_breast_cancer, load_diabetes
-from sklearn.model_selection import train_test_split
-
-
-# import necessary libraries and load the dataset
-from sklearn.linear_model import LinearRegression
-
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
-from explainerdashboard import RegressionExplainer, ClassifierExplainer, ExplainerDashboard, ExplainerHub
-import pandas as pd
-
 
 # Global Variable
 if 'started' not in st.session_state:
